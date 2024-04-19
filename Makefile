ab48ebbd663abd345c2fab7619ca3f38dad86c92
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-04-19 15:31:03 +0200

    Reconfigured files

--- Makefile
+++ Makefile
@@ -6,7 +6,17 @@ else
 	CFLAGS=-g -G -std=c++17
 endif
 
-all: bin/xva
+ifeq ($(OS), Windows_NT)
+	DEL=del /Q
+else
+	DEL=rm -f
+endif
+
+all: linux windows doc
+
+linux: bin/xva
+
+windows: bin/xva.exe
 
 bin/xva: obj/main.o
 	$(CC) $(CFLAGS) -o $@ $^
@@ -14,9 +24,15 @@ bin/xva: obj/main.o
 obj/main.o: src/main.cpp
 	$(CC) $(CFLAGS) -o $@ -c $<
 
+bin/xva.exe: obj/main.obj
+	$(CC) $(CFLAGS) -o $@ $^
+
+obj/main.obj: src/main.cpp
+	$(CC) $(CFLAGS) -o $@ -c $<
+
 doc:
 	doxygen Doxyfile
 
 clean:
-	rm -f obj/*.o bin/xva
-	rm -rf Documentation/*
+	$(DEL) obj/*.o* bin/xva bin/xva.exe
+	$(DEL) -r -v Documentation/*
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,79 @@
+/**
+ * @file main.cpp
+ * @author Thomas Roiseux (thomas.roiseux@mathquantlab.com)
+ * @brief Main file
+ * @version 1.0
+ * @date 2024-04-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include <iostream>
 
 using namespace std;
 
 /**
+ * @brief Application information
+ *
+ * @param name Application name
+ */
+void info(const char *name)
+{
+    cout << "Usage: " << name << " [options]" << endl;
+    cout << "Options:" << endl;
+    cout << "  -h, --help      Display this information" << endl;
+    cout << "  -v, --version   Display application version" << endl;
+}
+
+/**
  * @brief Main function
- * 
+ *
  * @param argc Number of arguments
  * @param argv Arguments
- * @return int 
+ * @return int
  */
 int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        info(argv[0]);
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
+        {
+            info(argv[0]);
+            return 0;
+        }
+        else if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--version"))
+        {
+            cout << "Version: 1.0.0" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "Unknown option: " << argv[i] << endl;
+            return 1;
+        }
+    }
+
     try
     {
         cout << "Hello, World!" << endl;
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << endl;
         return 1;
     }
-    catch(...)
+    catch (...)
     {
         std::cerr << "Unknown exception" << endl;
         return 1;
     }
-    
+
     return 0;
 }
