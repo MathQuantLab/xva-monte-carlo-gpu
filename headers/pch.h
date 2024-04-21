e70c054708f398d38c1d241c7ebc837d906435ef
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-04-21 11:11:44 +0000

    Added PCH

---
+++ headers/pch.h
@@ -0,0 +1,15 @@
+/**
+ * @file pch.h
+ * @author Thomas Roiseux (thomas.roiseux@mathquantlab.com)
+ * @brief Precompiled header
+ * @version 0.1
+ * @date 2024-04-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include <cuda.h>
+#include <cuda_runtime.h>
---
+++ src/pch.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file pch.cpp
+ * @author Thomas Roiseux (thomas.roiseux@mathquantlab.com)
+ * @brief Precompiled header
+ * @version 0.1
+ * @date 2024-04-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../headers/pch.h"
