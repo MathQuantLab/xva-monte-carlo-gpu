1870c643b0717010170cad18ddc7ab9d25aae846
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-04-21 11:02:07 +0000

    Updated doc

--- Documentation/html/search/all_1.js
+++ Documentation/html/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['info_2',['info',['../main_8cpp.html#a40721f4c45df5720145db897c012070c',1,'main.cpp']]],
-  ['is_5fgpu_5favailable_3',['is_gpu_available',['../cuda__utils_8h.html#ac3172c311c588e95a83276e2841906af',1,'cuda_utils.h']]]
+  ['is_5fgpu_5favailable_3',['is_gpu_available',['../cuda__utils_8h.html#af8c38737713c2a0edf070c9348cb87b0',1,'cuda_utils.h']]]
 ];
