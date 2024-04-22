bf4a63a739c3dba7444eec4e8f95d6a387f414e7
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-04-22 08:45:56 +0000

    Added CUDA simulation files

---
+++ headers/cuda_simulation.h
@@ -0,0 +1,26 @@
+/**
+ * @file cuda_simulation.h
+ * @author Thomas Roiseux (thomas.roiseux@mathquantlab.com)
+ * @brief Holds functions to run simulation on GPU
+ * @version 0.1
+ * @date 2024-04-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include "../headers/pch.h"
+
+namespace CUDA
+{
+    /**
+     * @brief Provides simulation functions on CUDA
+     * 
+     */
+    namespace Simulation
+    {
+
+    }
+}
---
+++ src/cuda_simulation.cu
@@ -0,0 +1,12 @@
+/**
+ * @file cuda_simulation.cu
+ * @author Thomas Roiseux (thomas.roiseux@mathquantlab.com)
+ * @brief Implements {@link cuda_simulation.h}
+ * @version 0.1
+ * @date 2024-04-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../headers/cuda_simulation.h"
