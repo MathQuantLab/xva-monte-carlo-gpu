e28d06b6b19271b96b501a728bf7b926e60b40d3
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-04-27 13:30:54 +0000

    Updated version

--- src/nmc.cpp
+++ src/nmc.cpp
@@ -2,7 +2,7 @@
  * @file nmc.cpp
  * @author Thomas Roiseux (thomas.roiseux@@mathquantlab.com)
  * @brief Implements {@link nmc.h}
- * @version 0.1
+ * @version 1.0
  * @date 2024-04-23
  *
  * @copyright Copyright (c) 2024
