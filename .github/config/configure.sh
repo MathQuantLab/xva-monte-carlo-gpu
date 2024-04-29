f51af639e423f3f4c02d3da761a93cdb993222e5
Thomas Roiseux <thomas.roiseux@outlook.com>
2024-04-29 10:29:34 +0200

    Upated pipeline

--- .github/config/configure.sh
+++ .github/config/configure.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y nvidia-cuda-toolkit make gcc g++
+echo "Initiating config script..."
+which nvcc &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Command nvcc detected. No config needed"
+else
+    echo "Command not detected. Installing..."
+
+    sudo apt update
+    sudo apt install -y nvidia-cuda-toolkit make gcc g++
+
+    echo "Successfully installed required tools"
+fi
+
+exit 0
